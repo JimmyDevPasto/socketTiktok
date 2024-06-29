168f71ac3883a3526e623c2e7d9908c9d1bb5f13
Jimmy Rosero <80181029+JimmyDevPasto@users.noreply.github.com>
2024-06-29 00:45:12 -0500

    4 commit

--- index.js
+++ index.js
@@ -10,17 +10,27 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: `*`, // Ajusta según donde esté tu frontend ${process.env.FRONTEND}
+    origin: 'https://667f9cb8e6cd03f885647c33--polite-gumption-7f8fcb.netlify.app',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
   }
 });
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`Servidor está corriendo en http://localhost:${port}`);
 });
 
 let tiktokLiveConnection;
+
+// Configurar CORS para Express
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://667f9cb8e6cd03f885647c33--polite-gumption-7f8fcb.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 // Conexión de socket.io
 io.on('connection', (socket) => {
@@ -40,7 +50,7 @@ io.on('connection', (socket) => {
 
     tiktokLiveConnection.connect().then(state => {
       console.log(`Conectado a la transmisión en vivo: ${state.roomId}`);
-      socket.emit('liveStatus', { success: true, message: 'Estás en vivo.' }); // aqui es cuando se conecta el usuario 
+      socket.emit('liveStatus', { success: true, message: 'Estás en vivo.' });
     }).catch(err => {
       console.error('Error al conectar con TikTok', err);
       socket.emit('liveStatus', { success: false, message: 'No estás haciendo en vivo, haz un live para conectarte.' });
@@ -74,4 +84,3 @@ io.on('connection', (socket) => {
 
   // Puedes añadir más manejo de eventos según tus necesidades
 });
-
